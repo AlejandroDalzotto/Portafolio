8795cff39bbd7326f2d87ee09f156b6d82e43c1c
AlejandroDalzotto <aledalzotto15@gmail.com>
2023-02-19 19:53:53 -0300

    se quita el destructuring

--- app/projects/[id]/page.jsx
+++ app/projects/[id]/page.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Link from "next/link.js"
 import Image from "next/image.js"
 import EyeIcon from "public/icons/eye-solid.svg"
@@ -262,22 +260,22 @@ const projects = [
 
 export default function Project({ params }) {
     const { id } = params
-    const { name, description, repository, contributors, frameworks, languages, demo, resources } = projects[id - 1]
+    const project = projects[id - 1]
 
     return (
         <div className="min-h-[calc(110vh+80px)] px-4">
             <div>
-                <h2 className="text-center my-6 text-4xl font-bold text-slate-50">{name}</h2>
-                <p className="text-center my-6 font-medium text-slate-50">{description}</p>
+                <h2 className="text-center my-6 text-4xl font-bold text-slate-50">{project.name}</h2>
+                <p className="text-center my-6 font-medium text-slate-50">{project.description}</p>
                 <div className="flex overflow-x-auto p-4 h-80 gap-4 md:gap-6 md:justify-center">
                     <div className="ring-1 ring-white/[0.15] bg-[radial-gradient(145.05%_100%_at_50%_0%,#334e68_0%,#243b53_57.38%,#102a43_88.16%)] text-slate-50 p-4 shadow rounded-xl min-w-[80%] md:min-w-[20%] transition-all lg:hover:min-w-[45%]">
                         <h3 className="text-xl font-semibold">Tecnologías</h3>
                         <hr className="h-1 my-2 w-full bg-slate-50 text-slate-50" />
                         <div className="w-full h-[calc(100%-16px-28px)] grid grid-cols-2 lg:grid-cols-3 content-start justify-items-stretch">
-                            {languages.map((el) => (
+                            {project.languages.map((el) => (
                                 <p key={el} className="text-slate-300">{el}</p>
                             ))}
-                            {frameworks.map((el) => (
+                            {project.frameworks.map((el) => (
                                 <p key={el} className="text-slate-300">{el}</p>
                             ))}
                         </div>
@@ -286,7 +284,7 @@ export default function Project({ params }) {
                         <h3 className="text-xl font-semibold">Recursos</h3>
                         <hr className="h-1 my-2 w-full bg-slate-50 text-slate-50" />
                         <div className="w-full h-[calc(100%-16px-28px)] grid grid-cols-2 lg:grid-cols-3 content-start justify-items-stretch">
-                            {resources.map((el) => (
+                            {project.resources.map((el) => (
                                 <p key={el} className="text-slate-300">{el}</p>
                             ))}
                         </div>
@@ -295,7 +293,7 @@ export default function Project({ params }) {
 
                 <h2 className="text-xl text-center font-semibold my-4 text-slate-50">Contribuidores</h2>
                 <div className="my-6 px-4 grid grid-cols-2 md:grid-cols-3 gap-8 md:justify-items-center">
-                    {contributors.map((el) => (
+                    {project.contributors.map((el) => (
                         <div key={el.name} className="ring-1 ring-white/[0.15] bg-[radial-gradient(145.05%_100%_at_50%_0%,#334e68_0%,#243b53_57.38%,#102a43_88.16%)] text-slate-50 p-4 shadow rounded-xl w-44 lg:w-60">
                             <h3 className="text-lg font-semibold tracking-tight">{el.name}</h3>
                             <hr className="h-1 my-2 w-full bg-slate-50 text-slate-50" />
@@ -305,14 +303,14 @@ export default function Project({ params }) {
                 </div>
             </div>
 
-            {(demo !== null || repository !== null) ?
+            {(project.demo !== null || project.repository !== null) ?
                 <div className="bg-[#050a12] h-20 flex items-center justify-evenly fixed bottom-0 left-0 right-0">
-                    {demo !== null ?
-                        <Link href={demo} target='_blank' className="-translate-y-10 ring-1 ring-white/[0.15] bg-[radial-gradient(145.05%_100%_at_50%_0%,#1D2B41_0%,#0B1627_57.38%,#142133_88.16%)] text-slate-50 p-4 text-4xl w-16 h-16 flex items-center justify-center rounded-full">
+                    {project.demo !== null ?
+                        <Link href={project.demo} target='_blank' className="-translate-y-10 ring-1 ring-white/[0.15] bg-[radial-gradient(145.05%_100%_at_50%_0%,#1D2B41_0%,#0B1627_57.38%,#142133_88.16%)] text-slate-50 p-4 text-4xl w-16 h-16 flex items-center justify-center rounded-full">
                             <Image src={EyeIcon} className="invert" alt="icon" />
                         </Link> : null}
-                    {repository !== null ?
-                        <Link href={repository} target='_blank' className="-translate-y-10 ring-1 ring-white/[0.15] bg-[radial-gradient(145.05%_100%_at_50%_0%,#1D2B41_0%,#0B1627_57.38%,#142133_88.16%)] text-slate-50 p-4 w-16 h-16 flex items-center justify-center rounded-full">
+                    {project.repository !== null ?
+                        <Link href={project.repository} target='_blank' className="-translate-y-10 ring-1 ring-white/[0.15] bg-[radial-gradient(145.05%_100%_at_50%_0%,#1D2B41_0%,#0B1627_57.38%,#142133_88.16%)] text-slate-50 p-4 w-16 h-16 flex items-center justify-center rounded-full">
                             <Image src={GithubIcon} className="invert" alt="icon" />
                         </Link> : null}
                 </div>
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Image from "next/image"
 import Link from "next/link"
 import DefaultImage from "../../public/imgs/default-image-project.jpg"
@@ -261,15 +259,15 @@ const projects = [
 export default function ProjectsPage() {
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center w-full">
-      {projects.map(({ id, name, img, short_description }) => (
-        <div key={id} className="ring-1 ring-white/[0.15] bg-gray-900 text-slate-50 w-72 h-96 shadow-lg m-4 p-4 rounded relative">
-          <Image className="rounded-t" src={!img ? DefaultImage : img} alt={name} width={260} height={192} />
-          <h3 className="font-semibold my-3 text-xl">{name}</h3>
-          <p>{short_description}</p>
-          <Link href='/projects/[id]' as={`/projects/${id}`} className="py-2 px-2 bg-indigo-500 rounded block text-center text-white font-semibold absolute bottom-4 right-4 left-4">Ver info</Link>
-        </div>
+    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center w-full">
+      {projects.map((project) => (
+        <article key={project.id} className="ring-1 ring-white/[0.15] bg-gray-900 text-slate-50 w-72 h-96 shadow-lg m-4 p-4 rounded relative">
+          <Image className="rounded-t" src={!project.img ? DefaultImage : project.img} alt={project.name} width={260} height={192} />
+          <h3 className="font-semibold my-3 text-xl">{project.name}</h3>
+          <p>{project.short_description}</p>
+          <Link href='/projects/[id]' as={`/projects/${project.id}`} className="py-2 px-2 bg-indigo-500 rounded block text-center text-white font-semibold absolute bottom-4 right-4 left-4">Ver info</Link>
+        </article>
       ))}
-    </div>
+    </section>
   )
 }
