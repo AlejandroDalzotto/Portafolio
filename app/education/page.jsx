9b524b39267f456874623f942bfce5d597efd1e5
AlejandroDalzotto <aledalzotto15@gmail.com>
2023-02-19 22:39:45 -0300

    Ya

--- app/education/page.jsx
+++ app/education/page.jsx
@@ -1,33 +1,28 @@
 import DiplomaIcon from "@/public/icons/diploma.png"
 import Image from "next/image"
 import Link from "next/link"
+import { EDUCATION } from "@/data"
 
-const education = [
-  {
-    id: 1,
-    title: 'Técnico en Programación',
-    description: 'Esta carrera me ofrecio conocimientos y prácticas intensivas en diferentes tecnologías que me permiten analizar un problema de procesamiento de datos y desarrollar una solución en un lenguaje apropiado.',
-    link: 'https://files.deply.dev/files/baa83db5-e885-4385-94de-a1b939b0b07c-520ceb42-5a7b-41c3-ab63-71bf903de69a-certificado.jpg'
-  }
-]
 
 export default function EducationPage() {
   return (
     <div>
       <div className="flex flex-col items-center">
         <h2 className="bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,_#7ED4FD_14.06%,#709DF7_51.02%,#4D78EF_79.09%)] bg-clip-text text-5xl font-bold leading-[1.2] tracking-wider text-transparent text-center my-6">Educación</h2>
-        {education.map(({ id, title, description, link }) => (
+        {EDUCATION.map((item, idx) => {
+          return (
 
-          <div key={id} className="ring-1 ring-white/[0.15] bg-gray-900 text-slate-50 w-72 h-96 shadow-lg m-4 p-4 rounded relative">
-            <Link href={link} target="_blank" className="absolute top-4 right-4">
-              <Image src={DiplomaIcon} alt="icon" width={30} height={30} className="invert" />
-            </Link>
-            <h2 className="text-slate-50 font-medium tracking-tight text-2xl">{title}</h2>
-            <hr className="bg-slate-50 h-1 w-full my-6" />
-            <p className="text-slate-400">{description}</p>
-          </div>
+            <div key={idx} className="ring-1 ring-white/[0.15] bg-gray-900 text-slate-50 w-72 h-96 shadow-lg m-4 p-4 rounded relative">
+              <Link href={item.link} target="_blank" className="absolute top-4 right-4">
+                <Image src={DiplomaIcon} alt="icon" width={30} height={30} className="invert" />
+              </Link>
+              <h2 className="text-slate-50 font-medium tracking-tight text-2xl">{item.title}</h2>
+              <hr className="bg-slate-50 h-1 w-full my-6" />
+              <p className="text-slate-400">{item.description}</p>
+            </div>
 
-        ))}
+          )
+        })}
       </div>
     </div>
   )
--- data.js
+++ data.js
@@ -2,24 +2,24 @@ import React from "react"
 
 export const PROJECTS = [
     {
-        "id": 1,
-        "name": "StockTracking",
-        "short_description": "Aplicación Full stack para el control de Stock.",
-        "description": "Es una aplicación que nos permite realizar el seguimiento de Stock de la medicina usada en un Tambo.",
-        "repository": "https://github.com/WaldoCuevas/StockTracking",
-        "languages": [
+        id: 1,
+        project_name: "StockTracking",
+        short_description: "Aplicación Full stack para el control de Stock.",
+        description: "Es una aplicación que nos permite realizar el seguimiento de Stock de la medicina usada en un Tambo.",
+        repository: "https://github.com/WaldoCuevas/StockTracking",
+        languages: [
             "Java",
             "TypeScript",
             "JavaScript",
             "HTML",
             "CSS"
         ],
-        "frameworks": [
+        frameworks: [
             "Spring boot",
             "Spring Security",
             "Angular"
         ],
-        "resources": [
+        resources: [
             "Toastr",
             "JWT",
             "Auth 2.0",
@@ -32,155 +32,155 @@ export const PROJECTS = [
             "Font Awesome",
             "Google Fonts"
         ],
-        "contributors": [
+        contributors: [
             {
-                "name": "Waldo Cuevas",
-                "profile": "https://github.com/WaldoCuevas"
+                username: "Waldo Cuevas",
+                profile: "https://github.com/WaldoCuevas"
             },
             {
-                "name": "Alejandro Dalzotto",
-                "profile": "https://github.com/alejandrodalzotto"
+                username: "Alejandro Dalzotto",
+                profile: "https://github.com/alejandrodalzotto"
             }
         ],
-        "demo": null,
-        "img": "https://images.unsplash.com/photo-1533305120462-aed774147024?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwxMnx8Y291bnRyeXxlbnwwfHx8fDE2NzY4MTc3MTU&ixlib=rb-4.0.3&q=80&w=1080"
+        demo: null,
+        img: "https://images.unsplash.com/photo-1533305120462-aed774147024?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwxMnx8Y291bnRyeXxlbnwwfHx8fDE2NzY4MTc3MTU&ixlib=rb-4.0.3&q=80&w=1080"
     },
     {
-        "id": 2,
-        "name": "Comunitate",
-        "short_description": "PWA que ayuda a mejorar la comunicación.",
-        "description": "Es una aplicación web progresiva que permite a los usuarios en condiciones especiales mejorar su comunicacíon.",
-        "repository": "https://github.com/lM4nu/Comunicate",
-        "languages": [
+        id: 2,
+        project_name: "Comunitate",
+        short_description: "PWA que ayuda a mejorar la comunicación.",
+        description: "Es una aplicación web progresiva que permite a los usuarios en condiciones especiales mejorar su comunicacíon.",
+        repository: "https://github.com/lM4nu/Comunicate",
+        languages: [
             "TypeScript",
             "JavaScript",
             "HTML",
             "CSS"
         ],
-        "frameworks": [
+        frameworks: [
             "Angular"
         ],
-        "resources": [
+        resources: [
             "Angular PWA",
             "LocalStorage",
             "Google Fonts",
             "Font Awesome",
             "SpeechSynthesisUtterance"
         ],
-        "contributors": [
+        contributors: [
             {
-                "name": "Waldo Cuevas",
-                "profile": "https://github.com/WaldoCuevas"
+                username: "Waldo Cuevas",
+                profile: "https://github.com/WaldoCuevas"
             },
             {
-                "name": "Alejandro Dalzotto",
-                "profile": "https://github.com/alejandrodalzotto"
+                username: "Alejandro Dalzotto",
+                profile: "https://github.com/alejandrodalzotto"
             },
             {
-                "name": "Manuel Sosa",
-                "profile": "https://github.com/lM4nu"
+                username: "Manuel Sosa",
+                profile: "https://github.com/lM4nu"
             }
         ],
-        "demo": "https://comunicate-con-nosotros.web.app/home",
-        "img": "https://images.unsplash.com/photo-1588072432836-e10032774350?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwzfHxzY2hvb2x8ZW58MHx8fHwxNjc2ODE4MTY0&ixlib=rb-4.0.3&q=80&w=1080"
+        demo: "https://comunicate-con-nosotros.web.app/home",
+        img: "https://images.unsplash.com/photo-1588072432836-e10032774350?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwzfHxzY2hvb2x8ZW58MHx8fHwxNjc2ODE4MTY0&ixlib=rb-4.0.3&q=80&w=1080"
     },
     {
-        "id": 3,
-        "name": "Carrito de compras",
-        "short_description": "Aplicación que simula un comportamiento de una web Ecommerce.",
-        "description": "Una pequeña aplicación que muestra una serie de productos los cuales se puede agregar a un carro.",
-        "repository": "https://github.com/AlejandroDalzotto/carrito-de-compras",
-        "languages": [
+        id: 3,
+        project_name: "Carrito de compras",
+        short_description: "Aplicación que simula un comportamiento de una web Ecommerce.",
+        description: "Una pequeña aplicación que muestra una serie de productos los cuales se puede agregar a un carro.",
+        repository: "https://github.com/AlejandroDalzotto/carrito-de-compras",
+        languages: [
             "JavaScript",
             "HTML",
             "CSS"
         ],
-        "frameworks": [
+        frameworks: [
             "React",
             "TailwindCSS"
         ],
-        "resources": [
+        resources: [
             "Font Awesome",
             "Aos library"
         ],
-        "contributors": [
+        contributors: [
             {
-                "name": "Alejandro Dalzotto",
-                "profile": "https://github.com/alejandrodalzotto"
+                username: "Alejandro Dalzotto",
+                profile: "https://github.com/alejandrodalzotto"
             }
         ],
-        "demo": "https://alejandrodalzotto.github.io/carrito-de-compras/",
-        "img": "https://images.unsplash.com/photo-1557821552-17105176677c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwxNXx8c2hvcHBpbmd8ZW58MHx8fHwxNjc2ODE4MzIw&ixlib=rb-4.0.3&q=80&w=1080"
+        demo: "https://alejandrodalzotto.github.io/carrito-de-compras/",
+        img: "https://images.unsplash.com/photo-1557821552-17105176677c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwxNXx8c2hvcHBpbmd8ZW58MHx8fHwxNjc2ODE4MzIw&ixlib=rb-4.0.3&q=80&w=1080"
     },
     {
-        "id": 4,
-        "name": "Gestor de Usuarios",
-        "short_description": "Un pequeño C.R de Usuarios.",
-        "description": "Un pequeño proyecto con React para practicar el manejo de estado de los formularios.",
-        "repository": "https://github.com/AlejandroDalzotto/gestor-de-usuarios",
-        "languages": [
+        id: 4,
+        project_name: "Gestor de Usuarios",
+        short_description: "Un pequeño C.R de Usuarios.",
+        description: "Un pequeño proyecto con React para practicar el manejo de estado de los formularios.",
+        repository: "https://github.com/AlejandroDalzotto/gestor-de-usuarios",
+        languages: [
             "JavaScript",
             "HTML",
             "CSS"
         ],
-        "frameworks": [
+        frameworks: [
             "React",
             "Tailwind"
         ],
-        "resources": [
+        resources: [
             "Google Fonts"
         ],
-        "contributors": [
+        contributors: [
             {
-                "name": "Alejandro Dalzotto",
-                "profile": "https://github.com/alejandrodalzotto"
+                username: "Alejandro Dalzotto",
+                profile: "https://github.com/alejandrodalzotto"
             }
         ],
-        "demo": null,
-        "img": false
+        demo: null,
+        img: false
     },
     {
-        "id": 5,
-        "name": "My galery",
-        "short_description": "Un buscador de imágenes.",
-        "description": "Un pequeño proyecto con React el cual nos permite hacer llamados a la API de Unsplash y nos itera una pequeña cantidad de imágenes como resultado de nuestra búsqueda.",
-        "repository": "https://github.com/AlejandroDalzotto/galeria-de-imagenes",
-        "languages": [
+        id: 5,
+        project_name: "My galery",
+        short_description: "Un buscador de imágenes.",
+        description: "Un pequeño proyecto con React el cual nos permite hacer llamados a la API de Unsplash y nos itera una pequeña cantidad de imágenes como resultado de nuestra búsqueda.",
+        repository: "https://github.com/AlejandroDalzotto/galeria-de-imagenes",
+        languages: [
             "JavaScript",
             "HTML",
             "CSS"
         ],
-        "frameworks": [
+        frameworks: [
             "React",
             "TailwindCSS"
         ],
-        "resources": [
+        resources: [
             "Unsplash API",
             "Font Awesome",
             "Aos Librery"
         ],
-        "contributors": [
+        contributors: [
             {
-                "name": "Alejandro Dalzotto",
-                "profile": "https: //github.com/alejandrodalzotto"
+                username: "Alejandro Dalzotto",
+                profile: "https: //github.com/alejandrodalzotto"
             }
         ],
-        "demo": "https://alejandrodalzotto.github.io/galeria-de-imagenes/",
-        "img": "https://images.unsplash.com/photo-1523461811963-7f1023caeddd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHw2fHxpbWFnZXN8ZW58MHx8fHwxNjc2ODE4NDg0&ixlib=rb-4.0.3&q=80&w=1080"
+        demo: "https://alejandrodalzotto.github.io/galeria-de-imagenes/",
+        img: "https://images.unsplash.com/photo-1523461811963-7f1023caeddd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHw2fHxpbWFnZXN8ZW58MHx8fHwxNjc2ODE4NDg0&ixlib=rb-4.0.3&q=80&w=1080"
     },
     {
-        "id": 6,
-        "name": "Lista Series",
-        "short_description": "Un C.R.U.D para controlar que series estamos viendo.",
-        "description": "Es una aplicación web donde podemos agregar series/películas/animes para controlar cuantos episodios vimos y así no perdernos.",
-        "repository": null,
-        "languages": [
+        id: 6,
+        project_name: "Lista Series",
+        short_description: "Un C.R.U.D para controlar que series estamos viendo.",
+        description: "Es una aplicación web donde podemos agregar series/películas/animes para controlar cuantos episodios vimos y así no perdernos.",
+        repository: null,
+        languages: [
             "Java"
         ],
-        "frameworks": [
+        frameworks: [
             "Spring boot"
         ],
-        "resources": [
+        resources: [
             "Google Fonts",
             "Font Awesome",
             "Maven",
@@ -188,68 +188,77 @@ export const PROJECTS = [
             "MySQL",
             "JPA"
         ],
-        "contributors": [
+        contributors: [
             {
-                "name": "Alejandro Dalzotto",
-                "profile": "https://github.com/alejandrodalzotto"
+                username: "Alejandro Dalzotto",
+                profile: "https://github.com/alejandrodalzotto"
             }
         ],
-        "demo": null,
-        "img": "https://images.unsplash.com/photo-1560169897-fc0cdbdfa4d5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwxMHx8bmV0ZmxpeHxlbnwwfHx8fDE2NzY4MTg2MDI&ixlib=rb-4.0.3&q=80&w=1080"
+        demo: null,
+        img: "https://images.unsplash.com/photo-1560169897-fc0cdbdfa4d5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwxMHx8bmV0ZmxpeHxlbnwwfHx8fDE2NzY4MTg2MDI&ixlib=rb-4.0.3&q=80&w=1080"
     },
     {
-        "id": 7,
-        "name": "Calculadora",
-        "short_description": "Calculadora de interés compuesto.",
-        "description": "Este es un proyecto hecho con React que nos da una calculadora de interés compuesto. Esto lo que hace es calcular cuanto dinero (En dolares) nosotros vamos a ganar en el transcurso de los años si es que nosotros hacemos un deposito inicial y luego seguimos contribuyendo dinero de manera anual.",
-        "repository": "https://github.com/AlejandroDalzotto/calculadora-interes-compuesto",
-        "languages": [
+        id: 7,
+        project_name: "Calculadora",
+        short_description: "Calculadora de interés compuesto.",
+        description: "Este es un proyecto hecho con React que nos da una calculadora de interés compuesto. Esto lo que hace es calcular cuanto dinero (En dolares) nosotros vamos a ganar en el transcurso de los años si es que nosotros hacemos un deposito inicial y luego seguimos contribuyendo dinero de manera anual.",
+        repository: "https://github.com/AlejandroDalzotto/calculadora-interes-compuesto",
+        languages: [
             "JavaScript",
             "HTML",
             "CSS"
         ],
-        "frameworks": [
+        frameworks: [
             "React",
             "TailwindCSS"
         ],
-        "resources": [
+        resources: [
             "Styled-components librery",
             "Formik"
         ],
-        "contributors": [
+        contributors: [
             {
-                "name": "Alejandro Dalzotto",
-                "profile": "https://github.com/alejandrodalzotto"
+                username: "Alejandro Dalzotto",
+                profile: "https://github.com/alejandrodalzotto"
             }
         ],
-        "demo": null,
-        "img": "https://images.unsplash.com/photo-1626266061368-46a8f578ddd6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwxfHxjYWxjdWxhdG9yfGVufDB8fHx8MTY3NjgxODY1Mw&ixlib=rb-4.0.3&q=80&w=1080"
+        demo: null,
+        img: "https://images.unsplash.com/photo-1626266061368-46a8f578ddd6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwxfHxjYWxjdWxhdG9yfGVufDB8fHx8MTY3NjgxODY1Mw&ixlib=rb-4.0.3&q=80&w=1080"
     },
     {
-        "id": 8,
-        "name": "Portfolio Web",
-        "short_description": "Mi página personal.",
-        "description": "Esta es mi página web personal hecha con Next.js 13 pensada para que quienes estén interesados en conocerme puedan encontrar todo lo relacionado con mi experiencia como desarrollador en un solo lugar..",
-        "repository": "https://github.com/AlejandroDalzotto/Portafolio",
-        "languages": [
+        id: 8,
+        project_name: "Portfolio Web",
+        short_description: "Mi página personal.",
+        description: "Esta es mi página web personal hecha con Next.js 13 pensada para que quienes estén interesados en conocerme puedan encontrar todo lo relacionado con mi experiencia como desarrollador en un solo lugar..",
+        repository: "https://github.com/AlejandroDalzotto/Portafolio",
+        languages: [
             "JavaScript",
             "HTML",
             "CSS"
         ],
-        "frameworks": [
+        frameworks: [
             "Next.JS",
             "TailwindCSS"
         ],
-        "resources": [
+        resources: [
             "Vercel"
         ],
-        "contributors": [
+        contributors: [
             {
-                "name": "Alejandro Dalzotto",
-                "profile": "https://github.com/alejandrodalzotto"
+                username: "Alejandro Dalzotto",
+                profile: "https://github.com/alejandrodalzotto"
             }
         ],
-        "demo": null,
-        "img": "https://images.unsplash.com/photo-1621839673705-6617adf9e890?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwxMnx8d2ViJTIwZGVzaWdufGVufDB8fHx8MTY3NjgxODcwNA&ixlib=rb-4.0.3&q=80&w=1080"
+        demo: null,
+        img: "https://images.unsplash.com/photo-1621839673705-6617adf9e890?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MTAyMTV8MHwxfHNlYXJjaHwxMnx8d2ViJTIwZGVzaWdufGVufDB8fHx8MTY3NjgxODcwNA&ixlib=rb-4.0.3&q=80&w=1080"
+    }
+]
+
+export const EDUCATION = [
+    {
+        id: 1,
+        title: 'Técnico en Programación',
+        description: 'Esta carrera me ofrecio conocimientos y prácticas intensivas en diferentes tecnologías que me permiten analizar un problema de procesamiento de datos y desarrollar una solución en un lenguaje apropiado.',
+        link: 'https://files.deply.dev/files/baa83db5-e885-4385-94de-a1b939b0b07c-520ceb42-5a7b-41c3-ab63-71bf903de69a-certificado.jpg'
     }
 ]
