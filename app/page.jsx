c84217897e6f75c6d0451b1a5befb0bce3bd6aa3
AlejandroDalzotto <aledalzotto15@gmail.com>
2023-05-02 20:05:49 -0300

    updated resume

--- app/page.jsx
+++ app/page.jsx
@@ -30,7 +30,7 @@ export default function Home() {
                 <h2 className="bg-gradient-to-r from-amber-200 to-yellow-500 bg-clip-text text-4xl font-bold leading-[1.1] text-transparent text-center md:text-left">Portafolio de Presentación</h2>
                 <p className="font-medium text-lg xl:text-xl text-center md:text-left max-w-[50ch] text-[#cfdbd5]">Saludos 👋 mi nombre es Alejandro Dalzotto y me desempeño como desarrollador Full Stack.</p>
                 <div className='flex justify-center gap-1 md:gap-5 w-full md:justify-start'>
-                  <Link className='grid place-content-center md:text-xl text-neutral-900 font-bold py-1 px-4 bg-[#f5cb5c] border-[#f5cb5c] shadow border-2 transition-all rounded hover:border-[#f5cb5c] hover:bg-transparent hover:text-[#f5cb5c]' href="https://files.deply.dev/files/136730d9-c719-429b-98e6-6f9c10ea58a3-6d3d78b7-8bd0-49fe-81f9-b860efa4420e-CV_es.pdf">¡Échale un vistazo a mi CV!</Link>
+                  <Link className='grid place-content-center md:text-xl text-neutral-900 font-bold py-1 px-4 bg-[#f5cb5c] border-[#f5cb5c] shadow border-2 transition-all rounded hover:border-[#f5cb5c] hover:bg-transparent hover:text-[#f5cb5c]' href="https://files.deply.dev/files/fc3762a7-52b1-48e1-8e93-4e3ccbce7fa8-31a3dca2-714f-4413-9e2b-db647816a12e-CV_en.pdf">¡Échale un vistazo a mi CV!</Link>
                   <Link className='grid place-content-center md:text-xl text-[#f5cb5c] font-bold py-1 px-4 bg-transparent border-2 border-[#f5cb5c] transition-all rounded hover:border-[#f5cb5c] shadow hover:bg-[#f5cb5c] hover:text-neutral-900' href="mailto:aledalzotto15@gmail.com">¡Contáctame!</Link>
                 </div>
                 <div className="flex justify-center gap-5 w-full md:justify-start">
